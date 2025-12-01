2d52f5bc7502cfe7e6dd7badf10051a74c81ef3a
Arzu Caner <arzuguneycaner@gmail.com>
2025-12-01 21:35:40 +0000

    Add JavaScript array question with solution

---
+++ Day_1.js
@@ -0,0 +1,17 @@
+// Question
+Given the JavaScript code below, what will be the final value of result?
+const arr = [1, 2, 3, 4, 5];
+
+const result = arr
+  .filter(n => n % 2 === 1)   // keep odd numbers
+  .map(n => n * 2)            // double them
+  .reduce((sum, n) => sum + n, 0); // sum them
+
+console.log(result);
+
+// Solution
+result will be 18.
+
+filter keeps odd numbers: [1, 3, 5]
+map doubles them: [2, 6, 10]
+reduce sums them: 2 + 6 + 10 = 18
